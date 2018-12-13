091f7d342331d8cfccecfd15ba9f4c87030af20b
Mkim <sp3dng9@gmail.com>
2018-12-12 16:19:58 -0800

    functional

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,70 +1,65 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var cTable = require('console.table');
-
+//require mysql and inquirer
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+//create connection to db
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Thibaudcat9",
   database: "bamazon_DB"
-});
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
-  
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-    //   connection.end();
-    });
-  }
-  // validateInput makes sure that the user is supplying only positive integers for their inputs
-function validateInput(value) {
-	var integer = Number.isInteger(parseFloat(value));
-	var sign = Math.sign(value);
+})
 
-	if (integer && (sign === 1)) {
-		return true;
-	} else {
-		return 'Please enter a whole non-zero number.';
-	}
-}
+function start(){
+//prints the items for sale and their details
+connection.query('SELECT * FROM products', function(err, res){
+  if(err) throw err;
 
+  console.table(res);
+
+  console.log(' ');
   inquirer.prompt([
     {
       type: "input",
       name: "id",
       message: "What is the ID of the product you would like to purchase?",
-      validate: validateInput,
-	  filter: Number
+      validate: function(value){
+        if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+          return true;
+        } else{
+          return false;
+        }
+      }
     },
     {
       type: "input",
       name: "qty",
       message: "How much would you like to purchase?",
-      validate: validateInput,
-	  filter: Number
+      validate: function(value){
+        if(isNaN(value)){
+          return false;
+        } else{
+          return true;
+        }
       }
-    
+    }
     ]).then(function(ans){
-      var userPick = (ans.id);
-      var userQty = parseInt(ans.qty);
-      var grandTotal = parseFloat(((res[userPick].Price)*userQty).toFixed(2));
+      var whatToBuy = (ans.id)-1;
+      var howMuchToBuy = parseInt(ans.qty);
+      var grandTotal = parseFloat(((res[whatToBuy].price)*howMuchToBuy).toFixed(2));
 
       //check if quantity is sufficient
-      if(res[userPick].stock_quantity >= userQty){
+      if(res[whatToBuy].stock_quantity >= howMuchToBuy){
         //after purchase, updates quantity in Products
-        connection.query("UPDATE products SET ? WHERE ?", [
-        {stock_quantity: (res[userPick].stock_quantity - userQty)},
+        connection.query("UPDATE Products SET ? WHERE ?", [
+        {stock_quantity: (res[whatToBuy].stock_quantity - howMuchToBuy)},
         {item_id: ans.id}
         ], function(err, result){
             if(err) throw err;
             console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
         });
+
+        
 
       } else{
         console.log("Sorry, there's not enough in stock!");
@@ -72,7 +67,8 @@ function validateInput(value) {
 
       reprompt();
     })
-
+})
+}
 
 //asks if they would like to purchase another item
 function reprompt(){
@@ -82,11 +78,11 @@ function reprompt(){
     message: "Would you like to purchase another item?"
   }]).then(function(ans){
     if(ans.reply){
-        afterConnection();
+      start();
     } else{
       console.log("See you soon!");
     }
   });
 }
 
-// afterConnection();
+start();
